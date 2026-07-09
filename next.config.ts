5d165d6d4e58999e9418c5dbb2d958bac09f60e7
arosadoclud <arosado.blandino@gmail.com>
2026-07-09 17:56:53 -0400

    fix: whitelist Kingdom Studio R2 image domain (Next Image + CSP) (#5)

--- next.config.ts
+++ next.config.ts
@@ -32,7 +32,7 @@ const securityHeaders = [
       "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://pagead2.googlesyndication.com https://*.googlesyndication.com https://www.googletagmanager.com https://connect.facebook.net https://www.facebook.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
-      "img-src 'self' data: https://images.unsplash.com https://*.unsplash.com https://www.facebook.com https://www.google-analytics.com blob:",
+      "img-src 'self' data: https://images.unsplash.com https://*.unsplash.com https://pub-c35e24e567e64e278b36c6857a95ec25.r2.dev https://www.facebook.com https://www.google-analytics.com blob:",
       "connect-src 'self' https://*.googlesyndication.com https://www.google-analytics.com https://region1.google-analytics.com https://stats.g.doubleclick.net https://www.facebook.com https://connect.facebook.net",
       "frame-src 'self' https://pagead2.googlesyndication.com https://www.facebook.com",
       "object-src 'none'",
@@ -53,6 +53,10 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "*.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-c35e24e567e64e278b36c6857a95ec25.r2.dev",
       },
     ],
   },
